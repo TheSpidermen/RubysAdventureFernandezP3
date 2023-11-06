5abdc2b6034d4b3d0b815dbcd71d9d64eb379c0a
TheSpidermen <fernandezmarcus12@gmail.com>
2023-11-06 11:37:54 -0800

    Did something
    Tried fixing the file for ruby but it still doesn't work.

--- Ruby'sAdventureFernandezMarcus/Assets/Scripts/RubyController.cs
+++ Ruby'sAdventureFernandezMarcus/Assets/Scripts/RubyController.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+         horizontal = Input.GetAxis("horizontal");
+         vertical = Input.GetAxis("vertical");
+    }
 
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
-
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
